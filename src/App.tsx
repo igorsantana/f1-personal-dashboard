f59e05a2bb2872d5103f6d2e74bc0aba2d78294d
Igor Santana <igorpsant95@gmail.com>
2024-11-24 22:30:37 +0000

    remove stuff from main

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import { useState } from "react";
 import "./App.css";
+import Layout from "./components/app/layout";
 import { ThemeProvider } from "./components/theme-provider";
 import F1Provider from "./provider";
 
-export default function App({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
     <F1Provider>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        {children}
+        <Layout></Layout>
       </ThemeProvider>
     </F1Provider>
   );
